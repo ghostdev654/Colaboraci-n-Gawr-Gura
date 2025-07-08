094eb8f9696bf508421496285faa9763c9da3e41
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-07-08 09:49:49 -0500

    Add files via upload

---
+++ plugins/owner-salir.js
@@ -0,0 +1,22 @@
+
+let handler = async (m, { conn, text}) => {
+  const groupId = text? text: m.chat;
+
+  try {
+    const mensaje = `
+🌸 *Miku Bot se despide del grupo* 🌸
+
+👋 Ha sido un gusto estar con ustedes.
+`;
+
+    await conn.sendMessage(groupId, { text: mensaje});
+    await conn.groupLeave(groupId);
+} catch (e) {
+    console.error('🚫 Error al intentar abandonar el grupo:', e);
+    await m.reply('⚠️ Algo salió mal... No pude salir del grupo.');
+}
+};
+
+handler.command = /^(salir|leave|salirdelgrupo|leavegc)$/i;
+handler.group = true;
+export default handler;
