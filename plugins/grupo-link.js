738e9b3fa554b8ca88fce7d6f14a310f90e3f9ab
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-07-27 21:08:36 -0500

    Update grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -1,10 +1,22 @@
 var handler = async (m, { conn, args }) => {
+  let group = m.chat
+  let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '\t\t✿:･✧ link del grupo ✧･:✿\n\n\v' + link, m, { detectLink: true })
+  // Mensaje decorado al estilo Gawr Gura
+  const message = `
+✧･ﾟ: ✧･ﾟ: *「 ʜʏᴇᴇ~ 🦈 」* :･ﾟ✧ :･ﾟ✧
 
+🌊 *Aquí tienes el link del grupo buba~:* ✨
+
+➤ ${link}
+
+꒰ 🌟 *Únete y nada con nosotros en este hermoso grupo desu~!* 🌟 ꒱
+
+🌊 *- Gawr Gura 🦈*
+`
+  conn.reply(m.chat, message, m, { detectLink: true })
 }
+
 handler.help = ['link']
 handler.tags = ['grupo']
 handler.command = ['link', 'enlace']
