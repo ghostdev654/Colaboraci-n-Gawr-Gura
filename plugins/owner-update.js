98e65952877a5454f3236e79c902966b246a8167
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-11 09:21:44 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,25 +1,25 @@
 import { execSync } from 'child_process'
 
 let handler = async (m, { conn, text }) => {
-  await m.react('🕓') // ⏳ Espera...
+  await m.react('⏳') // ⏳ Espera...
 
   if (conn.user.jid == conn.user.jid) {
     try {
       let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
       await conn.reply(m.chat, `
-╭━━〔 *🌊 Azu~ Azu~ Actualización Shark* 〕━━⬣
-┃ *𓆩 🦈 Gawr Gura dice:* Yaa~ hice magia con el código~ ✨
+╭━━〔 *🖥️ Actualizaciones de Git* 〕━━⬣
+┃ 
 ┃
 ┃ *📥 Resultado:* 
 ┃ ${stdout.toString().trim().split('\n').map(l => `┃ ${l}`).join('\n')}
 ┃
-┃ *📌 Nota:* ¡Todo está fresquito desde GitHub~! 🐟
+┃ *📌 Nota:* Ya se actualizó a la última versión de Git
 ╰━━━━━━━━━━━━━━━━━━━━⬣`, m)
       await m.react('✅')
     } catch (e) {
       await conn.reply(m.chat, `
-╭❗ *Ayuuda~* 🐳
-┃ Ocurrió un error al actualizar~ 💥
+╭❗ *ERROR*
+┃ Ocurrió un error al actualizar.
 ┃ 
 ┃ *🧨 Error:* 
 ┃ ${e.message}
